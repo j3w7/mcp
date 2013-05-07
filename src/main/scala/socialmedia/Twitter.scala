ae02e9bd56f6574837d8556bba33d50ff9934301
j3w7 <jens.wissmann@gmx.net>
2013-05-07 17:37:45 +0200

    twitter tests

---
+++ src/main/scala/socialmedia/Twitter.scala
@@ -0,0 +1,5 @@
+package socialmedia
+
+object Twitter {
+
+}
--- src/test/scala/scallop/MinimalScallopTest.scala
+++ src/test/scala/scallop/MinimalScallopTest.scala
@@ -1,14 +1,31 @@
 package scallop
 
 import org.rogach.scallop.Scallop
+import org.rogach.scallop.ScallopOption
+import org.rogach.scallop.ScallopConf
+
+object Conf extends ScallopConf(List("-c", "3", "-E", "fruit=apple", "7.2")) {
+  // all options that are applicable to builder (like description, default, etc)
+  // are applicable here as well
+  val count: ScallopOption[Int] = opt[Int]("count", descr = "count the trees", required = true)
+    .map(1+) // also here work all standard Option methods -
+  // evaluation is deferred to after option construction
+  val properties = props[String]('E')
+  // types (:ScallopOption[Double]) can be omitted, here just for clarity
+  val size: ScallopOption[Double] = trailArg[Double](required = false)
+}
 
 object MinimalScallopTest extends App {
-  while (true) {
-    val cmd = readLine
-    val opts = Scallop()
-      .opt[Boolean]("apples")
-      .args(cmd.split(" "))
-      .verify
 
+  val cmd = readLine
+  // that's it. Completely type-safe and convenient.
+  assert(Conf.count() == 4)
+  assert(Conf.properties("fruit") == Some("apple"))
+  assert(Conf.size.get == Some(7.2))
+  // passing into other functions
+  def someInternalFunc(conf: Conf.type) {
+    assert(conf.count() == 4)
   }
+  someInternalFunc(Conf)
+
 }
